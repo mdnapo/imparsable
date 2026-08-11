f9b4ed2e1dc14d9191d8f4f17f8c9d6d90b4d7ff
Donne <mdd.napo@gmail.com>
2026-08-11 17:16:10 +0200

    Added Monaco editor

--- Imparsable.UI/src/app/app.config.ts
+++ Imparsable.UI/src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {provideMonacoEditor} from 'ngx-monaco-editor-v2';
+import monacoConfig from './app.config.monaco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideMonacoEditor(monacoConfig)
   ]
 };
--- Imparsable.UI/src/app/components/calculator-editor/calculator-editor.ts
+++ Imparsable.UI/src/app/components/calculator-editor/calculator-editor.ts
@@ -1,10 +1,29 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {EditorComponent} from 'ngx-monaco-editor-v2';
+import {editor} from 'monaco-editor';
+import IStandaloneEditorConstructionOptions = editor.IStandaloneEditorConstructionOptions;
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-calculator-editor',
-  imports: [],
+  imports: [
+    EditorComponent,
+    FormsModule
+  ],
   templateUrl: './calculator-editor.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './calculator-editor.scss',
 })
-export class CalculatorEditor {}
+export class CalculatorEditor {
+  code = `const pi = 3.14;
+const radius = 4 / 2;
+var area = 2 * pi * radius;
+print "Area" . ': ' . area;
+print 1 + "2";
+`
+
+  protected options: IStandaloneEditorConstructionOptions = {
+    language: 'clc',
+    automaticLayout: true,
+  };
+}
