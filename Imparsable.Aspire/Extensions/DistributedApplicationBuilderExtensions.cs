044a329f4dffe17a638a495acac7da69e610c2b7
Donne <mdd.napo@gmail.com>
2026-09-19 19:25:46 +0200

    Used fixed ports for some Aspire services

--- Imparsable.Aspire/Extensions/DistributedApplicationBuilderExtensions.cs
+++ Imparsable.Aspire/Extensions/DistributedApplicationBuilderExtensions.cs
@@ -11,29 +11,32 @@ public static class DistributedApplicationBuilderExtensions
             var prometheus = builder
                 .AddContainer("prometheus", "prom/prometheus")
                 .WithBindMount("./observability/prometheus.yml", "/etc/prometheus/prometheus.yml")
-                .WithHttpEndpoint(targetPort: 9090, name: "http");
+                .WithHttpEndpoint(port: 9090, targetPort: 9090, name: "http")
+                .WithUrlForEndpoint("http", url => url.DisplayText = "Prometheus");
+
+            builder
+                .AddContainer("grafana", "grafana/grafana")
+                .WithEnvironment("PROMETHEUS_ENDPOINT", prometheus.GetEndpoint("http"))
+                .WithBindMount("./observability/grafana/config", "/etc/grafana", isReadOnly: true)
+                .WithBindMount("./observability/grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
+                .WithHttpEndpoint(port: 3000, targetPort: 3000, name: "http")
+                .WithUrlForEndpoint("http", url => url.DisplayText = "Grafana")
+                .WaitFor(prometheus);
+
+            var jaeger = builder
+                .AddContainer("jaeger", "jaegertracing/jaeger")
+                .WithEnvironment("COLLECTOR_OTLP_ENABLED", "true")
+                .WithHttpEndpoint(port: 16686, targetPort: 16686, name: "http")
+                .WithEndpoint(targetPort: 4317, name: "otlp-grpc")
+                .WithUrlForEndpoint("http", url => url.DisplayText = "Jaeger");
 
             var collector = builder
                 .AddOpenTelemetryCollector("otel-collector")
                 .WithConfig("./observability/otel-collector.yml")
                 .WithEnvironment("ASPIRE_OTLP_ENDPOINT", builder.Configuration["ASPIRE_DASHBOARD_OTLP_ENDPOINT_URL"])
                 .WithEnvironment("PROMETHEUS_ENDPOINT", $"{prometheus.GetEndpoint("http")}/api/v1/otlp")
-                .WaitFor(prometheus);
-
-            var grafana = builder
-                .AddContainer("grafana", "grafana/grafana")
-                .WithEnvironment("PROMETHEUS_ENDPOINT", prometheus.GetEndpoint("http"))
-                .WithBindMount("./observability/grafana/config", "/etc/grafana", isReadOnly: true)
-                .WithBindMount("./observability/grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
-                .WithHttpEndpoint(targetPort: 3000, name: "http")
-                .WaitFor(collector);
-
-            var jaeger = builder
-                .AddContainer("jaeger", "jaegertracing/jaeger")
-                .WithEnvironment("COLLECTOR_OTLP_ENABLED", "true")
-                .WithHttpEndpoint(targetPort: 16686, name: "http")
-                .WithEndpoint(targetPort: 4317, name: "otlp-grpc")
-                .WaitFor(collector);
+                .WaitFor(prometheus)
+                .WaitFor(jaeger);
 
             var api = builder
                 .AddProject<Imparsable_API>("imparsable-api")
@@ -53,7 +56,7 @@ public static class DistributedApplicationBuilderExtensions
         {
             builder
                 .AddJavaScriptApp("imparsable-ui", "../Imparsable.UI", "start")
-                .WithUrl("http://localhost:4200", "UI")
+                .WithUrl("http://localhost:4200", "Imparsable UI")
                 .WaitFor(resources.Api)
                 ;
         }
